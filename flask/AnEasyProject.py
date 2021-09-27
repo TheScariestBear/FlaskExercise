d5f277d79d0707543c9583b1b7504474fa87fde0
TheScariestBear <91003487+TheScariestBear@users.noreply.github.com>
2021-09-26 17:58:32 -0700

    Update AnEasyProject.py

--- flask/AnEasyProject.py
+++ flask/AnEasyProject.py
@@ -4,14 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # This home page should have the form.
     return render_template('homepage.html')
 
-
-# This page will be the page after the form
 @app.route('/report')
 def report():
-    # Check the user name for the 3 requirements.
     username = request.args.get('username')
     lower = False
     upper = False
@@ -31,12 +27,6 @@ def report():
         return render_template('reportpass.html')
     else:
         return render_template('reportfail.html',number=number,upper=upper,lower=lower)
-    # HINTS:
-    # https://stackoverflow.com/questions/22997072/how-to-check-if-lowercase-letters-exist/22997094
-    # https://stackoverflow.com/questions/26515422/how-to-check-if-last-character-is-integer-in-raw-input
-
-    # Return the information to the report page html.
 
 if __name__ == '__main__':
-    # Fill this in!
     app.run(debug=True)
